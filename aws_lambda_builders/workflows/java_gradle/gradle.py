f774106feda5e4f99a0e56045000d5c4c5d6d83a
Lucas <12496191+lucashuy@users.noreply.github.com>
2024-10-08 10:52:16 -0700

    Update gradle.py

--- aws_lambda_builders/workflows/java_gradle/gradle.py
+++ aws_lambda_builders/workflows/java_gradle/gradle.py
@@ -36,8 +36,8 @@ class SubprocessGradle(object):
         args = ["build", "--build-file", build_file, "--debug", "--no-build-cache"]
         # if cache_dir is not None:
             # args.extend(["--project-cache-dir", cache_dir])
-        # if properties is not None:
-            # args.extend(["-D%s=%s" % (n, v) for n, v in properties.items()])
+        if properties is not None:
+            args.extend(["-D%s=%s" % (n, v) for n, v in properties.items()])
         if init_script_path is not None:
             args.extend(["--init-script", init_script_path])
         ret_code, _, stderr = self._run(args, source_dir)
